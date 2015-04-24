eceea2b91ba3c25e2fd6d22e94e97d5e7ba82119
James Harris <james.harris@mfwbooks.com>
2015-04-24 09:07:58 -0500

    Outlined person was jaggy. Filled-in person looks much nicer.

--- toy.js
+++ toy.js
@@ -1,24 +1,23 @@
 var canvas = document.getElementById("FamilyLearningCycleToy");
 var brush = canvas.getContext("2d");
-//brush.fillRect(50,25,150,100);
 
-function drawCircle(x, y, radius) {
+function drawCircle(x, y, radius, color) {
 	brush.beginPath();
 	brush.arc(x, y, radius, 0, Math.PI*2, false);
 	brush.closePath();
-	brush.strokeStyle = "#000";
-	brush.stroke();
+	brush.fillStyle = color;
+	brush.fill();
 }
-function drawHalfCircle(x, y, radius, facingUp) {
+function drawHalfCircle(x, y, radius, facingUp, color) {
 	brush.beginPath();
 	if (facingUp)
 		brush.arc(x, y, radius, 0, Math.PI, false);
 	else
 		brush.arc(x, y, radius, Math.PI, 0, false);
-	brush.strokeStyle = "#000";
-	brush.stroke();
+	brush.fillStyle = color;
+	brush.fill();
 }
-function drawPerson(x, y, height, lineColor) {
+function drawPerson(x, y, height, color) {
 	var WIDTH = 50;
 	var HEAD_RADIUS = WIDTH / 2;
 	var shoulders = 	{ x: x, 						y: y+HEAD_RADIUS*2.5 			};
@@ -27,25 +26,15 @@ function drawPerson(x, y, height, lineColor) {
 	var thighs = 		{ x: shoulders.x, 				y: shoulders.y + height 		};
 	thighs.left = 		{ x: shoulders.left.x, 			y: shoulders.left.y + height 	};
 	thighs.right =	 	{ x: shoulders.right.x, 		y: shoulders.right.y + height 	};
-	brush.strokeStyle = lineColor;
+	brush.fillStyle = color;
 
-	drawCircle(x, y, HEAD_RADIUS); // head
-	drawHalfCircle(shoulders.x, shoulders.y, HEAD_RADIUS, false); // shoulders
-	
-	// left edge
-	brush.moveTo(shoulders.left.x, shoulders.left.y);
-	brush.lineTo(thighs.left.x, thighs.left.y);
-	brush.stroke();
-
-	// right edge
-	brush.moveTo(shoulders.right.x, shoulders.right.y);
-	brush.lineTo(thighs.right.x, thighs.right.y);
-	brush.stroke();
-
+	drawCircle(x, y, HEAD_RADIUS, color); // head
+	drawHalfCircle(shoulders.x, shoulders.y, HEAD_RADIUS, false, color); // shoulders
+	brush.fillRect(shoulders.left.x, shoulders.left.y, WIDTH, height);
 	drawHalfCircle(thighs.x, thighs.y, HEAD_RADIUS, true); // groin
 }
 
-drawPerson(26, 25.5, 75, "#000");
+drawPerson(26, 25.5, 60, "#C5B7DA");
 
 
 
