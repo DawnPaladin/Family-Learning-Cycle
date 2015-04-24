5c5be38a2234115775a12e2f63384f485d8fdb00
James Harris <james.harris@mfwbooks.com>
2015-04-24 09:08:58 -0500

    Draggable figurine

--- toy.js
+++ toy.js
@@ -1,6 +1,9 @@
 var canvas = document.getElementById("FamilyLearningCycleToy");
 var brush = canvas.getContext("2d");
 
+var PERSON_WIDTH = 50;
+var debugging = false;
+
 function drawCircle(x, y, radius, color) {
 	brush.beginPath();
 	brush.arc(x, y, radius, 0, Math.PI*2, false);
@@ -18,8 +21,7 @@ function drawHalfCircle(x, y, radius, facingUp, color) {
 	brush.fill();
 }
 function drawPerson(x, y, height, color) {
-	var WIDTH = 50;
-	var HEAD_RADIUS = WIDTH / 2;
+	var HEAD_RADIUS = PERSON_WIDTH / 2;
 	var shoulders = 	{ x: x, 						y: y+HEAD_RADIUS*2.5 			};
 	shoulders.left = 	{ x: shoulders.x - HEAD_RADIUS, y: shoulders.y 					};
 	shoulders.right = 	{ x: shoulders.x + HEAD_RADIUS, y: shoulders.y 					};
@@ -30,32 +32,47 @@ function drawPerson(x, y, height, color) {
 
 	drawCircle(x, y, HEAD_RADIUS, color); // head
 	drawHalfCircle(shoulders.x, shoulders.y, HEAD_RADIUS, false, color); // shoulders
-	brush.fillRect(shoulders.left.x, shoulders.left.y, WIDTH, height);
+	brush.fillRect(shoulders.left.x, shoulders.left.y, PERSON_WIDTH, height);
 	drawHalfCircle(thighs.x, thighs.y, HEAD_RADIUS, true); // groin
 }
 
-drawPerson(26, 25.5, 60, "#C5B7DA");
+//drawPerson(26, 25.5, 60, "#C5B7DA");
 
-
-
-
-
-
-
-
-
-
-
-
-/*function Draggable(x,y) {
+function Draggable(originalX, originalY, height, color) {
 	
 	var self = this;
-	self.x = x;
-	self.y = y;
-	self.gotoX = x;
-	self.gotoY = y;
+	self.x = originalX;
+	self.y = originalY;
+	//self.gotoX = x;
+	//self.gotoY = y;
 
 	var offsetX, offsetY;
 	var pickupX, pickupY;
-	// ...
-}*/
+
+	self.draw = function(newX, newY) {
+		brush.clearRect(0,0,canvas.width,canvas.height);
+		//brush.save();
+		//brush.translate(self.x, self.y);
+		drawPerson(newX, newY, height, color);
+		//brush.restore();
+	};
+	self.draw(self.x, self.y);
+
+	self.drag = function(newX, newY) {
+		self.x = newX;
+		self.y = newY;
+		self.draw(newX, newY);
+	};
+
+}
+
+var Bobby = new Draggable(76, 75.5, 60, "#C5B7DA");
+
+window.addEventListener("mousemove", function(event) {
+	var deltaX = Math.abs(Mouse.x - Bobby.x);
+	var deltaY = Math.abs(Mouse.y - Bobby.y);
+	if (deltaX < PERSON_WIDTH && deltaY < PERSON_WIDTH && Mouse.pressed) {
+		// drag handling
+		Bobby.drag(Mouse.x+0.5,Mouse.y+0.5);
+	}
+});
