66a3abe5ea9caa96284458ece616338371dd0137
James Harris <james.harris@mfwbooks.com>
2015-04-24 09:20:57 -0500

    Grade indicators, nameplates, orphanage

--- controller.js
+++ controller.js
@@ -1,8 +1,38 @@
-function newToken(name, grade, height, color) {
+function newToken(name, rawGrade, height, color) {
+	// process value from Grade dropdown
+	var gradeObj = { line2Size: "large" };
+	switch (rawGrade) {
+		case "Preschool":
+			gradeObj.line1 = "Pre-";
+			gradeObj.line2 = "school";
+			gradeObj.line2Size = "small";
+			break;
+		case "Pre-K":
+			gradeObj.line1 = "Pre-";
+			gradeObj.line2 = "K";
+			break;
+		case "Kindergarten":
+			gradeObj.line1 = "Kinder-";
+			gradeObj.line2 = "garten";
+			gradeObj.line2Size = "small";
+			break;
+		default:
+			gradeObj.line1 = "Grade";
+			gradeObj.line2 = rawGrade;
+			break;
+	}
+
 	var tokenIndex = "token" + tokenRegistry.tokenCount++;
-	var theToken = new Token(name, grade, height, color);
+	var theToken = new Token(name, gradeObj, height, color);
 	tokenRegistry[tokenIndex] = theToken;
-	drawNewToken(10, 200, height, color);
+	tokenRegistry[tokenIndex].canvasGroup = drawNewToken(10, 200, name, gradeObj, height, color, tokenIndex);
 }
 
-document.getElementById("addChildBtn").addEventListener("click", function(){ newToken("Applejack", "12th grade", 50, "orange");});
+//document.getElementById("addChildBtn").addEventListener("click", function(){ newToken("Applejack", "12", 50, "orange");});
+newToken("Twilight Sparkle", "1", 50, "#662D8A");
+
+function orphan(tokenIndex) {
+	console.log("Removing " + tokenIndex);
+	eraseToken(tokenRegistry[tokenIndex].canvasGroup);
+	tokenRegistry[tokenIndex] = {};
+}
--- view.js
+++ view.js
@@ -11,6 +11,16 @@ fabric.Image.fromURL('img/RTR.svg', setupPlatform, { left: 450, top: 500, select
 fabric.Image.fromURL('img/EXP.svg', setupPlatform, { left: 100, top: 500, selectable: false, });
 fabric.Image.fromURL('img/MOD.svg', setupPlatform, { left:  50, top: 300, selectable: false, });
 
+var orphanage = new fabric.Rect({
+	width: 972,
+	height: 100,
+	left: 0,
+	top: 700,
+	fill: "#F2F2F2",
+	selectable: false,
+});
+canvas.add(orphanage);
+
 function setupPlatform(image) {
 	canvas.add(image); 
 	canvas.sendToBack(image);
@@ -24,7 +34,7 @@ function setupPlatform(image) {
 	};
 }
 
-function drawNewToken(x, y, height, color) {
+function drawNewToken(x, y, name, gradeObj, height, color, tokenIndex) {
 	var head = new fabric.Circle({
 		radius: HEAD_RADIUS,
 		left: 0.5, // half-pixel offset to prevent fuzzy antialiasing
@@ -45,21 +55,48 @@ function drawNewToken(x, y, height, color) {
 		top: HEAD_RADIUS*2.5 + height,
 		left: 0.5,
 	});
-	var token = new fabric.Group([head, shoulders, torso, base], {
+	var nameplate = new fabric.Text(name, {
+		fontFamily: "Source Sans Pro",
+		fontSize: 20,
+		top: -28,
+		left: HEAD_RADIUS,
+		originX: "center",
+	});
+	var gradeLine1 = new fabric.Text(gradeObj.line1, {
+		fontFamily: "Source Sans Pro",
+		fontSize: 12,
+		top: HEAD_RADIUS*3.5,
+		left: HEAD_RADIUS,
+		originX: "center",
+	});
+	var gradeLine2 = new fabric.Text(gradeObj.line2, {
+		fontFamily: "Source Sans Pro",
+		fontSize: 12,
+		top: HEAD_RADIUS*4,
+		left: HEAD_RADIUS,
+		originX: "center",
+	});
+	var token = new fabric.Group([head, shoulders, torso, base, nameplate, gradeLine1, gradeLine2], {
 		left: x,
 		top: y,
 		fill: color,
 		originX: "center",
 		originY: "bottom",
 		hasBorders: false,
-		hasControls: false
+		hasControls: false,
+		index: tokenIndex,
 	});
+	gradeLine1.setColor("#ffffff");
+	gradeLine2.setColor("#ffffff");
+	if (gradeObj.line2Size == "large") {
+		gradeLine2.setFontSize(36);
+	}
 	canvas.add(token);
 	return token;
 }
 
-var Twilight = drawNewToken(26, 200, 60, "#662D8A");
-var Pinkie = drawNewToken(150, 200, 40, "#ED458D");
+//var Twilight = drawNewToken(26, 200, "Twilight Sparkle", { line1: "Grade", line2: "1", line2Size: "large" }, 60, "#662D8A", "token1");
+//var Pinkie = drawNewToken(150, 200, "Pinkie Pie", { line1: "Kinder-", line2: "garten", line2Size: "small" }, 40, "#ED458D", "token2");
 
 canvas.on('object:modified', dropToken);
 
@@ -72,4 +109,11 @@ function dropToken(options){
 			return true;
 		}
 	}
+	if (draggedToken.intersectsWithObject(orphanage)) {
+		orphan(draggedToken.index);
+	}
+}
+
+function eraseToken(token) {
+	canvas.remove(token);
 }
