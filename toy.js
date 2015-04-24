bcb4bf40064fbc2ef84c109a91f2023e215b655b
James Harris <james.harris@mfwbooks.com>
2015-04-24 09:16:10 -0500

    Drag Figurines Onto Platforms Simulator 2015

--- toy.js
+++ toy.js
@@ -5,6 +5,27 @@ var PERSON_WIDTH = 50;
 var HEAD_RADIUS = PERSON_WIDTH / 2;
 var debugging = false;
 
+var boardSpaces = [];
+
+fabric.Image.fromURL('img/ECC.svg', setupBoardSpace, { left: 250, top: 100, selectable: false, });
+fabric.Image.fromURL('img/CTG.svg', setupBoardSpace, { left: 500, top: 300, selectable: false, });
+fabric.Image.fromURL('img/RTR.svg', setupBoardSpace, { left: 450, top: 500, selectable: false, });
+fabric.Image.fromURL('img/EXP.svg', setupBoardSpace, { left: 100, top: 500, selectable: false, });
+fabric.Image.fromURL('img/MOD.svg', setupBoardSpace, { left:  50, top: 300, selectable: false, });
+
+function setupBoardSpace(image) {
+	canvas.add(image); 
+	canvas.sendToBack(image);
+	boardSpaces.push(image);
+
+	image.dock = function(figurine) {
+		console.log("Docking with " + image);
+		figurine.left = image.getCenterPoint().x;
+		figurine.top = image.getCenterPoint().y;
+		figurine.setCoords();
+	};
+}
+
 function addFigurine(x, y, height, color) {
 	var head = new fabric.Circle({
 		radius: HEAD_RADIUS,
@@ -18,19 +39,35 @@ function addFigurine(x, y, height, color) {
 		height: height,
 		top: HEAD_RADIUS*3.5
 	});
-	var thighs = new fabric.Circle({
+	var base = new fabric.Circle({
 		radius: HEAD_RADIUS,
 		top: HEAD_RADIUS*2.5 + height
 	});
-	var figurine = new fabric.Group([head, shoulders, torso, thighs], {
+	var figurine = new fabric.Group([head, shoulders, torso, base], {
 		left: x + 0.5, // half-pixel offset to prevent fuzzy antialiasing
 		top: y,
 		fill: color,
+		originX: "center",
+		originY: "bottom",
 		hasBorders: false,
 		hasControls: false
 	});
 	canvas.add(figurine);
+	return figurine;
 }
 
-addFigurine(26, 25, 60, "#C5B7DA");
-addFigurine(150, 15, 40, "seagreen");
+var Twilight = addFigurine(26, 200, 60, "#662D8A");
+var Pinkie = addFigurine(150, 200, 40, "#ED458D");
+
+canvas.on('object:modified', dropFigurine);
+
+function dropFigurine(options){
+	var draggedFigurine = options.target;
+	draggedFigurine.setCoords();
+	for (var i = 0; i < boardSpaces.length; i++) {
+		if (draggedFigurine.intersectsWithObject(boardSpaces[i])) { // adapted from http://fabricjs.com/intersection/
+			boardSpaces[i].dock(draggedFigurine);
+			return true;
+		}
+	}
+}
