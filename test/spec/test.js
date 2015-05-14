57d1aa82aff6ff549e9c1d9e13bd73cf979daf7a
James Harris <james.harris@mfwbooks.com>
2015-05-14 11:20:21 -0500

    Unsuccessfully test for the presence of variables defined in our custom JavaScript

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,17 @@
-/* global describe, it */
+/* global describe, it, assert, expect */
 
 (function () {
-  'use strict';
+	'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
-
-      });
-    });
-  });
+	describe("Check that variables can be seen", function() {
+		it('should have the flcToy master object', function () {
+			expect(flcToy).to.be.a('object');
+		});
+		it('should have flcToy.view', function () {
+			expect(flcToy.view).to.be.a('object');
+		});
+		it('should have flcToy.view.canvas', function () {
+			expect(flcToy.view.canvas).to.be.a('object');
+		});
+	});
 })();
