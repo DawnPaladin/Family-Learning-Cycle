0ae7ca0d15bfa9af1380eb3a3dc2a127d0250bf0
James Harris <james.harris@mfwbooks.com>
2015-05-14 16:41:43 -0500

    Finish up Robert's story

--- app/scripts/story.js
+++ app/scripts/story.js
@@ -12,7 +12,8 @@ var story = {
 				}
 			]
 		},
-		{ text: "Over the next few years, Robert advances to Kindergarten...", advance: true},
+		{ text: "Over the next few years, Robert advances from pre-K...", advance: true},
+		{ text: "...to Kindergarten...", advance: true },
 		{ text: "...then 1st grade...", advance: true },
 		{ text: "...then 2nd grade.", advance: true },
 		{ text: "Starting in 3rd grade, Robert moves from Discover to Investigate and begins the Family Learning Cycle. He'll continue this cycle until he's ready to begin 9th grade.", advance: true },
@@ -22,6 +23,8 @@ var story = {
 		{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
 		{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
 		{ text: "In 9th grade, Robert exits the Family Learning Cycle and begins high school.", advance: true },
+		{ text: "(Keep advancing Robert through high school.)", advance: true },
+		{ text: "(Keep advancing Robert through high school.)", advance: true },
 		{ text: "Robert graduates from high school and is ready to begin a career or leave for college.", advance: true }
 	],
 	currentPage: -1,
