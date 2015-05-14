db5e46994f02b4364ba0ea6e0235c458ae9820f2
James Harris <james.harris@mfwbooks.com>
2015-05-14 17:05:59 -0500

    Support for interchangeable stories

--- app/scripts/story.js
+++ app/scripts/story.js
@@ -1,38 +1,40 @@
-var storyBox = jQuery('#storyText');
 var story = {
-	pages: [
-		{ text: "Once upon a time..." },
-		{ 
-			text: "Let's imagine a family with one child named Robert. Robert's parents start him out in Preschool.", 
-			tokens: [
-				{
-					name: "Robert",
-					init: ["Robert", "0", 50, "#dd5b5a"],
-					platform: "platform0"
-				}
-			]
-		},
-		{ text: "Over the next few years, Robert advances from pre-K...", advance: true},
-		{ text: "...to Kindergarten...", advance: true },
-		{ text: "...then 1st grade...", advance: true },
-		{ text: "...then 2nd grade.", advance: true },
-		{ text: "Starting in 3rd grade, Robert moves from Discover to Investigate and begins the Family Learning Cycle. He'll continue this cycle until he's ready to begin 9th grade.", advance: true },
-		{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
-		{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
-		{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
-		{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
-		{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
-		{ text: "In 9th grade, Robert exits the Family Learning Cycle and begins high school.", advance: true },
-		{ text: "(Keep advancing Robert through high school.)", advance: true },
-		{ text: "(Keep advancing Robert through high school.)", advance: true },
-		{ text: "Robert graduates from high school and is ready to begin a career or leave for college.", advance: true }
-	],
+	library: {
+		Robert: [
+			{ 
+				text: "Let's imagine a family with one child named Robert. Robert's parents start him out in Preschool.", 
+				tokens: [
+					{
+						name: "Robert",
+						init: ["Robert", "0", 50, "#dd5b5a"],
+						platform: "platform0"
+					}
+				]
+			},
+			{ text: "Over the next few years, Robert advances from pre-K...", advance: true},
+			{ text: "...to Kindergarten...", advance: true },
+			{ text: "...then 1st grade...", advance: true },
+			{ text: "...then 2nd grade.", advance: true },
+			{ text: "Starting in 3rd grade, Robert moves from Discover to Investigate and begins the Family Learning Cycle. He'll continue this cycle until he's ready to begin 9th grade.", advance: true },
+			{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
+			{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
+			{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
+			{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
+			{ text: "(Keep clicking forward until you've advanced Robert into 9th grade.)", advance: true},
+			{ text: "In 9th grade, Robert exits the Family Learning Cycle and begins high school.", advance: true },
+			{ text: "(Keep advancing Robert through high school.)", advance: true },
+			{ text: "(Keep advancing Robert through high school.)", advance: true },
+			{ text: "Robert graduates from high school and is ready to begin a career or leave for college.", advance: true }
+		]
+	},
+	box: jQuery('#storyText'),
+	pages: null,
 	currentPage: -1,
 	tokenRegistry: {},
 	turnPageForward: function(){
 		var currentPage = story.pages[++story.currentPage];
 
-		storyBox.text(currentPage.text);
+		story.box.text(currentPage.text);
 
 		// if page has associated tokens, create them
 		if (Array.isArray(currentPage.tokens) || false) {
@@ -52,7 +54,7 @@ var story = {
 	turnPageBackward: function(){
 		var oldPage = story.pages[story.currentPage];
 		var currentPage = story.pages[--story.currentPage];
-		storyBox.text(currentPage.text);
+		story.box.text(currentPage.text);
 		if (Array.isArray(oldPage.token) || false) { // if pages has an associated token, orphan it
 			var tokenName = oldPage.token[0];
 			var tokenIndex = story.tokenRegistry[tokenName];
@@ -66,4 +68,7 @@ var story = {
 
 jQuery('#storyPrevBtn').click(story.turnPageBackward);
 jQuery('#storyNextBtn').click(story.turnPageForward);
-story.turnPageForward();
+setTimeout(function(){
+	story.pages = story.library.Robert;
+	story.turnPageForward();
+}, 100);
