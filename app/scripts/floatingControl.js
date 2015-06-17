f4bef48f72989eff1ac69f8365800c5890109359
James Harris <james.harris@mfwbooks.com>
2015-06-17 16:55:30 -0500

    CSS and JS for a control that floats over each canvas

---
+++ app/scripts/floatingControl.js
@@ -0,0 +1,18 @@
+function floatControl($control, $aor) { // Make a control float over the page, but only when a particular element (the Area of Responsibility) is onscreen
+	jQuery(window).scroll(function(){
+		var scrollTop = jQuery(window).scrollTop(); // distance between the top of the window and the top of the page
+		var screenHeight = Math.round(jQuery(window).height());
+		var theoreticalBoxPosition = scrollTop + screenHeight;
+		var aorOffset = Math.round($aor.offset().top);
+		var aorHeight = Math.round($aor.height());
+		var aorTop = aorOffset;
+		var aorBottom = aorOffset + aorHeight;
+		if (theoreticalBoxPosition > aorTop && theoreticalBoxPosition < aorBottom) {
+			$control.addClass('hover');
+		} else {
+			$control.removeClass('hover');
+		}
+	});
+}
+floatControl(jQuery('#Robert-toy-wrapper .storyShuttleBox'), jQuery('#Robert-toy-wrapper'));
+floatControl(jQuery('#Carpenter-toy-wrapper .storyShuttleBox'), jQuery('#Carpenter-toy-wrapper'));
