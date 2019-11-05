3fe93cb0b1844aa269e1950504e2555d345fbd9e
James Harris <computersherpa@gmail.com>
2019-11-04 20:49:11 -0600

    Fix paths; restore missing images

--- app/scripts/flcToy.js
+++ app/scripts/flcToy.js
@@ -1,7 +1,7 @@
 /* jshint latedef: false */
 /* globals flcModelFactory, flcViewFactory, flcControllerFactory, flcStoryFactory */
 
-var imgDir = 'images';
+var imgDir = '/app/images';
 
 function toyFactory() {
 
